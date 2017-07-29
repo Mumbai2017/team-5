adcc974de82384d9cddff209952ba267c8d21b1b
agarwalansh <anshul.ashok.agarwal@gmail.com>
2017-07-30 00:41:49 +0530

    dashboard comment tried

---
+++ lectureplan.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Dash Board</title>
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+
+    <link rel="stylesheet" href="webUIOne.css">
+</head>
+<body style="background-color: linen">
+<nav class="navbar navbar-inverse navbar-static-top" role="navigation" style="margin: 0px;">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-WDM-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">CEQUE</a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="bs-WDM-navbar-collapse-1">
+            <ul class="nav navbar-nav pull-right">
+                <li><a href="mentordashboard.php">Videos</a></li>
+                <li><a href="#">Logout</a></li>         <!-- Add the logout link -->
+            </ul>
+        </div>
+    </div>
+</nav>
