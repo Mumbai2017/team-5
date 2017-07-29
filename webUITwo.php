03bba099203d09aa7480038d6e40ad23a3b36df9
vinayakkini101 <vinayakkini101@gmail.com>
2017-07-29 23:49:32 +0530

    Removed web ui two js file

--- webUITwo.php
+++ webUITwo.php
@@ -14,7 +14,7 @@
 </head>
 
 <body>
-<!-- facebook comments -->
+	<!-- facebook comments -->
 <!-- 	<div id="fb-root"></div>
 	<script>(function(d, s, id) {
 		var js, fjs = d.getElementsByTagName(s)[0];
@@ -23,26 +23,26 @@
 		js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.10";
 		fjs.parentNode.insertBefore(js, fjs);
 	}(document, 'script', 'facebook-jssdk'));
-	</script> -->
+</script> -->
 
-	<div class="container-fluid">
+<div class="container-fluid">
 
-		<div class="row">
-			<div class="col-xs-4">
-				<a href="webUITwo.php">
-					<video style="width:100%" controls>
-						<source src=sample.mp4 type=video/mp4 />
-					</video>
-				</a>
-			</div>
-			
+	<div class="row">
+		<div class="col-xs-4">
+			<a href="webUITwo.php">
+				<video style="width:100%" controls>
+					<source src=sample.mp4 type=video/mp4 />
+				</video>
+			</a>
 		</div>
 
-		<div class="row outer-margin justify-content-end ">
-			<div class="col-xs-12">
-				<button class="btn btn-sm btn-primary" id="commentButton">Comment</button>
-			</div>
+	</div>
+
+	<div class="row outer-margin justify-content-end ">
+		<div class="col-xs-12">
+			<button class="btn btn-sm btn-primary" id="commentButton">Comment</button>
 		</div>
+	</div>
 
 		<!-- <div class="row justify-content-end">
 			<div class="col-xs-12">
@@ -52,8 +52,14 @@
 
 		<div class="row outer-margin">
 			<div class="col-xs-12" id="comments">
-			<!-- facebook comments -->
+				<!-- facebook comments -->
 				<!-- <div class="fb-comments" data-href="https://developers.facebook.com/docs/plugins/comments#configurator" data-numposts="5"></div> -->
+				
+				<!-- begin wwww.htmlcommentbox.com -->
+				<div id="HCB_comment_box"><a href="http://www.htmlcommentbox.com">Widget</a> is loading comments...</div>
+				<link rel="stylesheet" type="text/css" href="//www.htmlcommentbox.com/static/skins/bootstrap/twitter-bootstrap.css?v=0" />
+				<script type="text/javascript" id="hcb"> /*<!--*/ if(!window.hcb_user){hcb_user={};} (function(){var s=document.createElement("script"), l=hcb_user.PAGE || (""+window.location).replace(/'/g,"%27"), h="//www.htmlcommentbox.com";s.setAttribute("type","text/javascript");s.setAttribute("src", h+"/jread?page="+encodeURIComponent(l).replace("+","%2B")+"&opts=16862&num=10&ts=1501351997452");if (typeof s!="undefined") document.getElementsByTagName("head")[0].appendChild(s);})(); /*-->*/ </script>
+				<!-- end www.htmlcommentbox.com -->
 			</div>
 		</div>
 	</div>
