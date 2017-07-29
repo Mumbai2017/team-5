7b773a1ec9c785e547bedd450cf6efb35b2d3394
agarwalansh <anshul.ashok.agarwal@gmail.com>
2017-07-29 23:31:36 +0530

    Mentor Dashboard videos complete

--- mentordashboard.php
+++ mentordashboard.php
@@ -3,9 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Good Features</title>
+    <title>Dash Board</title>
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
     <link rel="stylesheet" href="style.css">
+
+    <link rel="stylesheet" href="webUIOne.css">
 </head>
 <body style="background-color: linen">
 <nav class="navbar navbar-inverse navbar-static-top" role="navigation" style="margin: 0px;">
@@ -22,12 +24,18 @@
 
         <div class="collapse navbar-collapse" id="bs-WDM-navbar-collapse-1">
             <ul class="nav navbar-nav pull-right">
-                <li><a href="#">Logout</a></li>
+                <li><a href="#">Lecture Plans</a></li> <!-- Add the lecture plan link -->
+                <li><a href="#">Logout</a></li>         <!-- Add the logout link -->
             </ul>
         </div>
     </div>
 </nav>
 
+<div class="container">
+    <div class="embed-responsive embed-responsive-16by9">
+        <iframe class="embed-responsive-item" src="http://www.youtube.com/embed/-u-j7uqU7sI"></iframe>  <!-- Fetch the video link from the database -->
+    </div>
+</div>
 
 <footer class="site-footer">
     <div class="container-fluid">
