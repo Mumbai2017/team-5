ee247b9b49871c27b11f222d59a894da71af2486
vinayakkini101 <vinayakkini101@gmail.com>
2017-07-30 03:29:20 +0530

    web 2

--- webUIOne.php
+++ webUIOne.php
@@ -46,6 +46,9 @@ if(isset($_POST['username']))   // getting the json data from php
 
 <body>
 	<div class="container-fluid">
+
+	<?php
+		?>
 		<div class="row outer-margin">
 			<div>
 				<a href="webUITwo.php"><h2>Video Name 1</h2></a>
@@ -54,12 +57,21 @@ if(isset($_POST['username']))   // getting the json data from php
 		<div class="row outer-margin">
 			<div>
 				<a href="webUITwo.php">
-				<video  loop  style="width:100%;" controls>
+					<video  loop  style="width:100%;" controls>
 						<source src=sample.mp4 type=video/mp4 />
 					</video>
 				</a>
 			</div>
-		</div>	
+		</div>
+
+
+		
+		<!-- begin wwww.htmlcommentbox.com -->
+				<div id="HCB_comment_box"><a href="http://www.htmlcommentbox.com">Widget</a> is loading comments...</div>
+				<link rel="stylesheet" type="text/css" href="//www.htmlcommentbox.com/static/skins/bootstrap/twitter-bootstrap.css?v=0" />
+				<script type="text/javascript" id="hcb"> /*<!--*/ if(!window.hcb_user){hcb_user={};} (function(){var s=document.createElement("script"), l=hcb_user.PAGE || (""+window.location).replace(/'/g,"%27"), h="//www.htmlcommentbox.com";s.setAttribute("type","text/javascript");s.setAttribute("src", h+"/jread?page="+encodeURIComponent(l).replace("+","%2B")+"&opts=16862&num=10&ts=1501351997452");if (typeof s!="undefined") document.getElementsByTagName("head")[0].appendChild(s);})(); /*-->*/ </script>
+				<!-- end www.htmlcommentbox.com -->
+
 	</div>
 </body>
 </html>
--- webUITwo.php
+++ webUITwo.php
@@ -55,11 +55,14 @@
 				<!-- facebook comments -->
 				<!-- <div class="fb-comments" data-href="https://developers.facebook.com/docs/plugins/comments#configurator" data-numposts="5"></div> -->
 
+				
+
+
 				<!-- begin wwww.htmlcommentbox.com -->
-				<div id="HCB_comment_box"><a href="http://www.htmlcommentbox.com">Widget</a> is loading comments...</div>
-				<link rel="stylesheet" type="text/css" href="//www.htmlcommentbox.com/static/skins/bootstrap/twitter-bootstrap.css?v=0" />
-				<script type="text/javascript" id="hcb"> /*<!--*/ if(!window.hcb_user){hcb_user={};} (function(){var s=document.createElement("script"), l=hcb_user.PAGE || (""+window.location).replace(/'/g,"%27"), h="//www.htmlcommentbox.com";s.setAttribute("type","text/javascript");s.setAttribute("src", h+"/jread?page="+encodeURIComponent(l).replace("+","%2B")+"&opts=16862&num=10&ts=1501351997452");if (typeof s!="undefined") document.getElementsByTagName("head")[0].appendChild(s);})(); /*-->*/ </script>
-				<!-- end www.htmlcommentbox.com -->
+		<div id="HCB_comment_box"><a href="http://www.htmlcommentbox.com">Widget</a> is loading comments...</div>
+		<link rel="stylesheet" type="text/css" href="//www.htmlcommentbox.com/static/skins/bootstrap/twitter-bootstrap.css?v=0" />
+		<script type="text/javascript" id="hcb"> /*<!--*/ if(!window.hcb_user){hcb_user={};} (function(){var s=document.createElement("script"), l=hcb_user.PAGE || (""+window.location).replace(/'/g,"%27"), h="//www.htmlcommentbox.com";s.setAttribute("type","text/javascript");s.setAttribute("src", h+"/jread?page="+encodeURIComponent(l).replace("+","%2B")+"&opts=16862&num=10&ts=1501351997452");if (typeof s!="undefined") document.getElementsByTagName("head")[0].appendChild(s);})(); /*-->*/ </script>
+		<!-- end www.htmlcommentbox.com -->	
 			</div>
 		</div>
 	</div>
