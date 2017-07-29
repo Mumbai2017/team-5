b969914d8bab533f563371b9471162a7fc8d3e23
agarwalansh <anshul.ashok.agarwal@gmail.com>
2017-07-29 23:18:19 +0530

    Mentor dashboard

---
+++ mentordashboard.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Good Features</title>
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body style="background-color: linen">
+<nav class="navbar navbar-inverse navbar-static-top" role="navigation" style="margin: 0px;">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-WDM-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">CEQUE</a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="bs-WDM-navbar-collapse-1">
+            <ul class="nav navbar-nav pull-right">
+                <li><a href="#">Logout</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+
+<footer class="site-footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-5">
+                <h3>Contact Address</h3>
+                <address>
+                    <b>Centre for Equity and Quality in Universal Education</b>
+                    C/3 Sunita Apartments, Gujjar Lane,
+                    Santacruz (W), Mumbai - 400054<br>
+                    Maharashtra, INDIA<br>
+                    Email: admin@ceque.org<br>
+                    Phone: +91-22-2605-3242
+                </address>
+            </div>
+        </div>
+        <div class="bottom-footer">
+            <div class="col-md-5">&copy; 2017 - 2018</div>
+        </div>
+</footer>
+
+
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+            <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+</body>
+</html>
