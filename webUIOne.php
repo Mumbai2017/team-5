fce508fa12d067d1964df32b36fed57e6f2f0b85
agarwalansh <anshul.ashok.agarwal@gmail.com>
2017-07-30 09:49:21 +0530

    web UI 1 final

--- webUIOne.php
+++ webUIOne.php
@@ -63,14 +63,14 @@ if(isset($_POST['username']))   // getting the json data from php
 	
 		<div class="row outer-margin">
 			<div>
-				<a href="webUITwo.php"><h2>Video Name 1</h2></a>
+				<a href="webUITwo.php"><h2>Sentences</h2></a>
 			</div>
 		</div>
 		<div class="row outer-margin">
 			<div>
 				<a href="webUITwo.php">
 					<video  loop  style="width:100%;" controls>
-						<source src=sample.mp4 type=video/mp4 />
+						<source src="video.mp4" type=video/mp4 />
 					</video>
 				</a>
 			</div>
@@ -80,14 +80,6 @@ if(isset($_POST['username']))   // getting the json data from php
 	}
 	?>
 
-
-		<div class="row outer-margin">
-		<!-- begin wwww.htmlcommentbox.com -->
-				<div id="HCB_comment_box"><a href="http://www.htmlcommentbox.com">Widget</a> is loading comments...</div>
-				<link rel="stylesheet" type="text/css" href="//www.htmlcommentbox.com/static/skins/bootstrap/twitter-bootstrap.css?v=0" />
-				<script type="text/javascript" id="hcb"> /*<!--*/ if(!window.hcb_user){hcb_user={};} (function(){var s=document.createElement("script"), l=hcb_user.PAGE || (""+window.location).replace(/'/g,"%27"), h="//www.htmlcommentbox.com";s.setAttribute("type","text/javascript");s.setAttribute("src", h+"/jread?page="+encodeURIComponent(l).replace("+","%2B")+"&opts=16862&num=10&ts=1501351997452");if (typeof s!="undefined") document.getElementsByTagName("head")[0].appendChild(s);})(); /*-->*/ </script>
-				<!-- end www.htmlcommentbox.com -->
-		</div>
 	</div>
 </body>
 </html>
