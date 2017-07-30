95d8dfd2ec41d10d67822f4b580f37efe586e91b
Rajeevkumar Yadav <rajeev11430@gmail.com>
2017-07-29 22:17:19 -0400

    for linking the video with particular teacher

---
+++ php/video_detail.php
@@ -0,0 +1,33 @@
+<?php  
+
+$teacherid="";
+$filename="";
+
+if(isset($_POST['username']))   // getting the json data from php 
+{
+	$obj = json_decode($_POST['username']);
+	//echo $obj->{'username'};
+	$teacherid=$obj->{'username'};
+}
+
+
+if(isset($_POST['file_name']))   // getting the json data from php 
+{
+	$obj = json_decode($_POST['file_name']);
+	//echo $obj->{'username'};
+	$filename=$obj->{'file_name'};
+}
+
+ $url="uploads/";
+ 
+ $con=mysqli_connect('localhost','root','','team-5');
+ $sql="Insert into video values(1,$filename,$teacherid,$url)";  //issue with the database cant help it. now can't help it. 
+
+ $result=mysqli_query($con,$sql);
+
+
+ header("HTTP/1.1 200 OK");
+ header("Status: 200 All rosy");
+
+
+?>
